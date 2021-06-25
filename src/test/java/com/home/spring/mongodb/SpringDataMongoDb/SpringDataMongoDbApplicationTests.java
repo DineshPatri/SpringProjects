45af45e38fc9b918807688784bfbba1e5eb0fa8d
Patri, Dinesh <Dinesh.Patri@cerner.com>
2021-06-25 19:40:33 +0530

    Initial check in for Spring Data MongoDB project

---
+++ src/test/java/com/home/spring/mongodb/SpringDataMongoDb/SpringDataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.spring.mongodb.SpringDataMongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
